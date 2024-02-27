0e3c63cce75610cbc6008eb97e8ce52c6b69e8c4
Khash <khashayar.m.003@gmail.com>
2024-02-28 00:56:45 +0330

    fixing home index

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,39 @@
+import { Col, Row } from "antd"
 
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Row 
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          CalenderUpcomingEvents
+        </Col>
+
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: '460px'
+          }}
+        >
+          DashboardDealsChart
+        </Col>
+      </Row>
+    </div>
   )
 }
 
