f631cd9ba2f15c606ed571a7d8f97cbf1fc1efa0
Khash <khashayar.m.003@gmail.com>
2024-02-28 01:56:25 +0330

    consumed data in upcoming events component

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,10 +1,35 @@
 import { CalendarOutlined } from "@ant-design/icons";
-import { Card, List } from "antd";
+import { Badge, Card, List } from "antd";
 import { Text } from "../text";
-import { useState } from "react";
+import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
+import { getDate } from "@/utilities/helpers";
+import { useList } from "@refinedev/core";
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
+import dayjs from "dayjs";
 
 const UpcomingEvents = () => {
-    const [isLoading, setIsLoading] = useState(true)
+
+  const { data, isLoading } = useList({
+    resource: "events",
+    pagination: { pageSize: 5 },
+    sorters: [
+      {
+        field: "startDate",
+        order: "asc",
+      },
+    ],
+    filters: [
+      {
+        field: "startDate",
+        operator: "gte",
+        value: dayjs().format("YYYY-MM-DD"),
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY,
+    },
+  });
+  // console.log(JSON.stringify(data))
   return (
     <Card
       style={{ height: "100%" }}
@@ -25,21 +50,50 @@ const UpcomingEvents = () => {
         </div>
       }
     >
-        {isLoading ? (
-            <List 
-                itemLayout="horizontal"
-                dataSource={Array.from({ length: 5 }).map((_, index) =>({
-                    id: index,
-                    
-                }))}
-            >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: index,
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <List
+          itemLayout="horizontal"
+          dataSource={data?.data || []}
+          renderItem={(item) => {
+            const renderDate = getDate(item.startDate, item.endDate);
 
-            </List>
-        ) : (
-            <List>
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Badge color={item.color} />}
+                  title={<Text size="xs">{renderDate}</Text>}
+                  description={
+                    <Text ellipsis={{ tooltip: true }} strong>
+                      {item.title}
+                    </Text>
+                  }
+                />
+              </List.Item>
+            );
+          }}
+        />
+      )}
 
-            </List>
-        )}
+      {!isLoading && data?.data.length === 0 && (
+        <span
+          style={{
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            height: "220px",
+          }}
+        >
+          No upcoming events
+        </span>
+      )}
     </Card>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,13 @@ import KanbanColumnSkeleton from "./skeleton/kanban";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 
-
-
-export {  UpcomingEvents, DealsChart, UpcomingEventsSkeleton, AccordionHeaderSkeleton, KanbanColumnSkeleton, ProjectCardSkeleton, LatestActivitiesSkeleton  }
+export {
+  UpcomingEvents,
+  DealsChart,
+  
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+};
